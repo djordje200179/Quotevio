dab88ca508eff38f60ad43a4260d0bb3b17193b4
Đorđe Milanović <djordje200179@gmail.com>
2023-03-14 19:45:28 +0100

    Redundant type removed

--- backend/storage/entities/quote.go
+++ backend/storage/entities/quote.go
@@ -1,12 +1,14 @@
 package entities
 
+import "time"
+
 type Quote struct {
 	Id uint `json:"id"`
 
 	Text   string `json:"text"`
 	Author string `json:"author"`
 
-	Created Date `json:"created"`
+	Created time.Time `json:"created"`
 
 	Likes    uint64 `json:"likes"`
 	Dislikes uint64 `json:"dislikes"`
