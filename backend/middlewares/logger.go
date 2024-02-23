7b87b6f17ed0fd285834a98385625e5d6544191e
Đorđe Milanović <djordje200179@gmail.com>
2024-02-23 19:24:33 +0100

    Middleware refactored

--- backend/middlewares/logger.go
+++ backend/middlewares/logger.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 	"os"
 	"slices"
-	"strconv"
-	"strings"
 )
 
 type logger struct {
@@ -15,35 +13,36 @@ type logger struct {
 	next http.Handler
 }
 
-type responseRecorder struct {
+type responseInterceptor struct {
 	http.ResponseWriter
 
 	status int
 	body   []byte
 }
 
-func (r *responseRecorder) WriteHeader(status int) {
+func (r *responseInterceptor) WriteHeader(status int) {
 	r.status = status
 	r.ResponseWriter.WriteHeader(status)
 }
 
-func (r *responseRecorder) Write(b []byte) (int, error) {
+func (r *responseInterceptor) Write(b []byte) (int, error) {
 	r.body = slices.Clone(b)
 	return r.ResponseWriter.Write(b)
 }
 
 func Log(next http.Handler) http.Handler {
 	return logger{
-		stdOut: log.New(os.Stdout, "", 0),
-		stdErr: log.New(os.Stderr, "", 0),
-		next:   next,
+		stdOut: log.New(os.Stdout, "", log.LstdFlags),
+		stdErr: log.New(os.Stderr, "", log.LstdFlags),
+
+		next: next,
 	}
 }
 
 func (l logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	l.stdOut.Println(r.Method, r.URL.Path, r.RemoteAddr)
 
-	response := &responseRecorder{ResponseWriter: w}
+	response := &responseInterceptor{ResponseWriter: w}
 	l.next.ServeHTTP(response, r)
 
 	statusGroup := response.status / 100
@@ -51,20 +50,8 @@ func (l logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case 2, 3:
 		return
 	case 4:
-		l.stdOut.Println(makeResponseMessage(response.status, response.body))
+		l.stdOut.Printf("(%d) %s\n", response.status, response.body)
 	case 5:
-		l.stdErr.Println(makeResponseMessage(response.status, response.body))
+		l.stdErr.Printf("(%d) %s\n", response.status, response.body)
 	}
-}
-
-func makeResponseMessage(statusCode int, body []byte) string {
-	var sb strings.Builder
-
-	sb.WriteByte('(')
-	sb.WriteString(strconv.Itoa(statusCode))
-	sb.WriteByte(')')
-	sb.WriteByte(' ')
-	sb.Write(body)
-
-	return sb.String()
 }
